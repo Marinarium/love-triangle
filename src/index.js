b5e642bc4a741f44051c01aaad3ef7d4058eae4f
Marina <schwmk@gmail.com>
2018-02-21 05:32:07 +0300

    100%

--- src/index.js
+++ src/index.js
@@ -3,33 +3,32 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  var lovers = [];
+	var lovers = [];
 	var loveTriangels = [];
+	var total = 0;
 	
 	for (i = 0; i < preferences.length; i++) {
-		//1
+
 		var value1 = preferences[i];  
-		//2	
+				
 		var i2 = preferences[value1 - 1];
 		var value2 = i2;
-		//3
+		
 		var i3 = preferences[value2 - 1];
 		var value3 = i3;
 		var i1 = preferences[value3 - 1];
 		
 		if ((value1 != value2) && (value2 != value3) && (value3 != value1) && (value1 == i1 )) {
-			lovers.splice(-1, 0, value1, value2, value3);
+			loveTriangels = loveTriangels.concat(value1, value2, value3);
 		}		
-	}	
-	
-	nextInput:
-        for (var x = 0; x < lovers.length; x++) {
-          var element = lovers[x]; // для каждого элемента
-          for (var y = 0; y < loveTriangels.length; y++) { // ищем, был ли он уже?
-            if (loveTriangels[y] == element) continue nextInput; // если да, то следующий
-          }
-          var total = loveTriangels.push(element)/3;
-        }
+	}
 		
+	for (i = 0; i < loveTriangels.length; i++) {
+	  if (lovers.indexOf(loveTriangels[i])== -1) {
+		lovers.push(loveTriangels[i]);
+		var total = lovers.length/3;
+	  }
+	}
+	
 	return total;
-}
+  }
